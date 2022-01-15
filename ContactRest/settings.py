32f1857f0db5c89787d093106c42b2d1fd566819
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2022-01-15 18:11:22 +0100

    cleanup -- moved code from bitbucket

--- ContactRest/settings.py
+++ ContactRest/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'btp-jn9ra3a9ljo_mjqr1$881=w&n%r*&ly3$xnbt0bdf5epc='
+# SECURITY WARNING: keep the secret key used in production secret! Update the key
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
